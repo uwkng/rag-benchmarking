32ca86f58fe471bd5f179caf1436002ebe8c146b
uwkng <koenig_u@gmx.net>
2025-09-10 01:07:29 +0200

    Add initial index.py: Getting chunked data, define model and create embeddings

---
+++ app/index.py
@@ -0,0 +1,15 @@
+import json
+from sentence_transformers import SentenceTransformer
+
+# Get the chunked data
+texts = []
+with open("data/chunks.jsonl", "r", encoding="utf-8") as f:
+    for line in f:
+        texts.append(json.loads(line)["text"])
+
+# Define the model
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# Create the embeddings
+embeddings = model.encode(texts)
+
